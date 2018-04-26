96d78253c05375d5d6ef5fd2ef16f38edb8d11c8
allongo <allongo@163.com>
2018-04-26 18:32:00 +0800

    dependency log

--- src/main/java/com/aog/controller/DemoController.java
+++ src/main/java/com/aog/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.aog.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("demo")
 public class DemoController {
+    private static final Logger log = LoggerFactory.getLogger(DemoController.class);
+
     @RequestMapping(value = "/hello",method= RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public String hello(){
+        log.info("logback init");
         return "hello SpringBoot";
     }
 }
